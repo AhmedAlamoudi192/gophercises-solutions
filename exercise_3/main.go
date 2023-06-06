e2e3e8135f37df0f7e70666ade284b1a36dc9851
AhmedAlamoudi192 <hamode192@gmail.com>
2023-06-07 00:48:22 +0300

    exercise 3 Done

--- exercise_3/main.go
+++ exercise_3/main.go
@@ -24,13 +24,23 @@ func checknil(e error) {
 	}
 }
 
+func registerRoute(mux *http.ServeMux, arc string, stories map[string]StoryArc) {
+	templ:=`<body style="text-align:center"><h1>{{.Title}}</h1>{{range .Story}}<p>{{.}}</p>{{end}}<div style="display:flex;gap:2rem;justify-content:center">{{range .Options}}<a href="{{.Arc}}">{{.Text}}</a>{{end}}</div></body>`
+	tmpl, err := template.New(arc).Parse(templ)
+	checknil(err)
+	if strings.Compare(arc, "intro")!=0 {	
+		mux.HandleFunc(fmt.Sprintf("/%s",arc), func(w http.ResponseWriter, h *http.Request) {
+			tmpl.Execute(w, stories[arc])
+		})
+	}else {
+		mux.HandleFunc("/", func(w http.ResponseWriter, h *http.Request) {
+			tmpl.Execute(w, stories[arc])
+		})
+	}
+}
+
 func httpHandler() *http.ServeMux {
 	mux := http.NewServeMux()
-	tmpl, err := template.New("Entry").Parse(`<h1>hello world</h1>`)
-	checknil(err)
-	mux.HandleFunc("/", func(w http.ResponseWriter, h *http.Request) {
-		tmpl.Execute(w, nil)
-	})
 	return mux
 }
 
@@ -38,10 +48,11 @@ func main() {
 	jsonBlob, os_err := os.ReadFile("./gopher.json")
 	checknil(os_err)
 	var stories map[string]StoryArc
-	json_err := json.Unmarshal(jsonBlob, &stories)
-	checknil(json_err)
-	fmt.Printf("%s\n", stories["intro"].Title)
-	fmt.Printf("%s\n", strings.Join(stories["intro"].Story, ""))
-	fmt.Printf("- %s\n", stories["intro"].Options[0].Text)
-	fmt.Printf("- %s\n", stories["intro"].Options[1].Text)
+	jsonErr := json.Unmarshal(jsonBlob, &stories)
+	checknil(jsonErr)
+	mux := httpHandler()
+	for k := range stories { 
+		registerRoute(mux,k,stories)
+	}
+	http.ListenAndServe("localhost:8080",mux)
 }
